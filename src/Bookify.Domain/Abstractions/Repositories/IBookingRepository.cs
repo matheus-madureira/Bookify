dcd9d4bbcd144644c05c710e8135eea2477cd103
Matheus Madureira <mattheuscontato17@gmail.com>
2026-02-22 11:15:52 -0300

    fix: entities domain errors and repositories

---
+++ src/Bookify.Domain/Abstractions/Repositories/IBookingRepository.cs
@@ -0,0 +1,16 @@
+﻿using Bookify.Domain.Apartments;
+using Bookify.Domain.Bookings;
+
+namespace Bookify.Domain.Abstractions.Repositories;
+
+public interface IBookingRepository
+{
+    Task<Booking?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+
+    Task<bool> IsOverlappingAsync(
+        Apartment apartment,
+        DateRange duration,
+        CancellationToken cancellationToken = default);
+
+    void Add(Booking booking);
+}
