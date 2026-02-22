ecf42e59275251f1428e5a0f6c09987ea2720b7a
Matheus Madureira <mattheuscontato17@gmail.com>
2026-02-22 12:21:12 -0300

    feat: get booking by id handler

---
+++ src/Bookify.Application/Abstractions/Data/Queries/BookingSqlQueries.cs
@@ -0,0 +1,25 @@
+﻿namespace Bookify.Application.Abstractions.Data.Queries;
+
+internal sealed class BookingSqlQueries
+{
+    public const string GetBookingById = """
+        SELECT
+            id AS Id,
+            apartment_id AS ApartmentId,
+            user_id AS UserId,
+            status AS Status,
+            price_for_period_amount AS PriceAmount,
+            price_for_period_currency AS PriceCurrency,
+            cleaning_fee_amount AS CleaningFeeAmount,
+            cleaning_fee_currency AS CleaningFeeCurrency,
+            amenities_up_charge_amount AS AmenitiesUpChargeAmount,
+            amenities_up_charge_currency AS AmenitiesUpChargeCurrency,
+            total_price_amount AS TotalPriceAmount,
+            total_price_currency AS TotalPriceCurrency,
+            duration_start AS DurationStart,
+            duration_end AS DurationEnd,
+            created_on_utc AS CreatedOnUtc
+        FROM bookings
+        WHERE id = @BookingId
+        """;
+}
---
+++ src/Bookify.Application/Bookings/GetBooking/GetBookingQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using Bookify.Application.Abstractions.Data;
+using Bookify.Application.Abstractions.Data.Queries;
+using Bookify.Application.Abstractions.Messaging;
+using Bookify.Domain.Abstractions;
+using Dapper;
+
+namespace Bookify.Application.Bookings.GetBooking;
+
+internal sealed class GetBookingQueryHandler(ISqlConnectionFactory sqlConnectionFactory)
+    : IQueryHandler<GetBookingQuery, BookingResponse>
+{
+    private readonly ISqlConnectionFactory _sqlConnectionFactory = sqlConnectionFactory;
+
+    public async Task<Result<BookingResponse>> Handle(GetBookingQuery request, CancellationToken cancellationToken)
+    {
+        using var connection = _sqlConnectionFactory.CreateConnection();
+
+        const string sql = BookingSqlQueries.GetBookingById;
+
+        var booking = await connection.QueryFirstOrDefaultAsync<BookingResponse>(
+            sql,
+            new { request.BookingId });
+
+        return booking;
+    }
+}
