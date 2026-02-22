942ba8f674a280d8d41b834028a4713a7cd4ff40
Matheus Madureira <mattheuscontato17@gmail.com>
2026-02-21 21:50:22 -0300

    fix: domain layer, apartment entity and value objects

---
+++ Bookify.Domain/Apartments/Apartment.cs
@@ -0,0 +1,27 @@
+﻿using Bookify.Domain.Abstractions;
+
+namespace Bookify.Domain.Apartments;
+
+public sealed class Apartment(
+    Guid id,
+    Name name,
+    Description description,
+    Address address,
+    Money price,
+    Money cleaningFee,
+    List<Amenity> amenities) : Entity(id)
+{
+    public Name Name { get; private set; }
+
+    public Description Description { get; private set; }
+
+    public Address Address { get; private set; }
+
+    public Money Price { get; private set; }
+
+    public Money CleaningFee { get; private set; }
+
+    public DateTime? LastBookedOnUtc { get; private set; }
+
+    public List<Amenity> Amenities { get; private set; } = [];
+}
