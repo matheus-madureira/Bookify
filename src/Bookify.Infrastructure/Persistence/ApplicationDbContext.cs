b3d7d9bd135937e08a4c8737d458800368941148
Matheus Madureira <mattheuscontato17@gmail.com>
2026-02-24 21:15:26 -0300

    fix: efcore setup in infrastructure layer

---
+++ src/Bookify.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Bookify.Domain.Abstractions.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookify.Infrastructure.Persistence;
+
+public sealed class ApplicationDbContext(DbContextOptions options)
+    : DbContext(options), IUnityOfWork
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
